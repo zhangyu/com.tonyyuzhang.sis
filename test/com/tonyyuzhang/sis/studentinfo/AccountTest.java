cc8503b7e180be40b18ac2904fba616778a455a2
Tony Zhang <zhangyu.main@gmail.com>
2011-06-22 22:58:38 +0100

    L12 anonymous class

--- test/com/tonyyuzhang/sis/studentinfo/AccountTest.java
+++ test/com/tonyyuzhang/sis/studentinfo/AccountTest.java
@@ -3,46 +3,100 @@ package com.tonyyuzhang.sis.studentinfo;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jimbob.ach.Ach;
+import com.jimbob.ach.AchCredentials;
+import com.jimbob.ach.AchResponse;
+import com.jimbob.ach.AchStatus;
+import com.jimbob.ach.AchTransactionData;
+
 public class AccountTest {
-	static final String ABA = "102000012";
-	static final String ACCOUNT_NUMBER = "1944321518811";
-	
-	private Account account;
-	
-	@Before
-	public void setUp() throws Exception {
-		account = new Account();
-		account.setBankAba(ABA);
-		account.setBankAccountNumber(ACCOUNT_NUMBER);
-		account.setBankAccountType(Account.BankAccountType.CHECKING);
-	}
-	
-	@Test
-	public void testTransactions() {
-		account.credit(new BigDecimal("0.10"));
-		account.credit(new BigDecimal("10.00"));
-		assertEquals(new BigDecimal("10.10"), account.getBalance());
-	}
-	@Test
-	public void testTransactionAverage() {
-		account.credit(new BigDecimal("0.10"));
-		account.credit(new BigDecimal("11.00"));
-		account.credit(new BigDecimal("2.99"));
-		account.credit(new BigDecimal("4.70"));
-		assertEquals(new BigDecimal("4.70"), account.getTransactionAverage());
-	}
-	
-	@Test
-	public void testTransferFromBank() {
-//		account.setAch(new com.jimbobach.ach.JimBobAch());
-		account.setAch(new MockAch());
-		final BigDecimal amount = new BigDecimal("50.00");
-		account.transferFromBank(amount);
-		
-		assertEquals(amount, account.getBalance());
-	}
+   static final String ABA = "102000012";
+   static final String ACCOUNT_NUMBER = "1944321518811";
+   
+   private Account account;
+   
+   @Before
+   public void setUp() throws Exception {
+      account = new Account();
+      account.setBankAba(ABA);
+      account.setBankAccountNumber(ACCOUNT_NUMBER);
+      account.setBankAccountType(Account.BankAccountType.CHECKING);
+   }
+   
+   @Test
+   public void testTransactions() {
+      account.credit(new BigDecimal("0.10"));
+      account.credit(new BigDecimal("10.00"));
+      assertEquals(new BigDecimal("10.10"), account.getBalance());
+   }
+   @Test
+   public void testTransactionAverage() {
+      account.credit(new BigDecimal("0.10"));
+      account.credit(new BigDecimal("11.00"));
+      account.credit(new BigDecimal("2.99"));
+      account.credit(new BigDecimal("4.70"));
+      assertEquals(new BigDecimal("4.70"), account.getTransactionAverage());
+   }
+   
+   @Test
+   public void testTransferFromBank() {
+//      account.setAch(new com.jimbobach.ach.JimBobAch());
+      Ach mockAch = new Ach() {
+
+         @Override
+         public AchResponse issueDebit(AchCredentials credentials,
+               AchTransactionData data) {
+            Assert.assertTrue(data.account.equals(AccountTest.ACCOUNT_NUMBER));
+            Assert.assertTrue(data.aba.equals(AccountTest.ABA));
+            
+            AchResponse response = new AchResponse();
+            response.timestamp = new Date();
+            response.traceCode = "1";
+            response.status = AchStatus.SUCCESS;
+            return response;
+         }
+
+         @Override
+         public AchResponse markTransactionAsNSF(AchCredentials credentials,
+               AchTransactionData data, String traceCode) {
+            return null;
+         }
+
+         @Override
+         public AchResponse refundTransaction(AchCredentials credentials,
+               AchTransactionData data, String traceCode) {
+            return null;
+         }
+
+         @Override
+         public AchResponse issueCredit(AchCredentials credentials,
+               AchTransactionData data) {
+            return null;
+         }
+
+         @Override
+         public AchResponse voidSameDayTransaction(AchCredentials credentials,
+               AchTransactionData data, String traceCode) {
+            return null;
+         }
+
+         @Override
+         public AchResponse queryTransaction(AchCredentials credentials,
+               AchTransactionData data, String traceCode) {
+            return null;
+         }
+
+      };
+      account.setAch(mockAch);
+      final BigDecimal amount = new BigDecimal("50.00");
+      account.transferFromBank(amount);
+      
+      assertEquals(amount, account.getBalance());
+   }
 }
